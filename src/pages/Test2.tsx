7cbbd539baafa12c5136f47c961f0779d44b08ca
Your Name <you@example.com>
2024-11-18 20:23:58 +0500

    Test2

---
+++ src/pages/Test2.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box, Toolbar, CssBaseline } from '@mui/material';
+import DrawerComponent from '../components/DrawerComponent';
+import HeaderComponent from '../components/HeaderComponent';
+
+function Test2() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <DrawerComponent />
+      <HeaderComponent />
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        {/* Main content can be added here later */}
+      </Box>
+    </Box>
+  );
+}
+
+export default Test2;
